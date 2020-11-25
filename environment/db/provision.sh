7edb01690f485ee0df82160f2bb784d0098d0404
D3v <deviljin112@users.noreply.github.com>
2020-11-25 14:21:39 +0000

    Init

---
+++ environment/db/provision.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org=3.2.20
+sudo cp /vagrant/mongod.conf /etc/
+sudo systemctl enable mongod.service --now
